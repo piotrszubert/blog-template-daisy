786dc3e9e89ae3f891dc7a42bd3bb1a3d55e0b57
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-11-22 08:40:36 +0000

    added theme toggle component

---
+++ app/components/theme-toggle.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import { useLocalStorage } from "@uidotdev/usehooks";
+
+type Theme = string | null;
+
+export const ThemeToggle = () => {
+    const [theme, setTheme] = useLocalStorage<Theme>("theme", null);
+
+    const themes = [
+        "default", "light", "dark", "cupcake", "bumblebee", "emerald", "corporate",
+        "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest",
+        "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula",
+        "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter",
+        "dim", "nord", "sunset"
+    ];
+
+    return(
+        <div className="join join-vertical">
+            {themes.map((themeOption => (
+                <input 
+                    key={themeOption }
+                    type="radio" 
+                    name="theme-buttons" 
+                    className="btn theme-controller join-item" 
+                    // checked={theme === themeOption}
+                    // capitalize theme name
+                    aria-label={themeOption.charAt(0).toUpperCase()+ themeOption.slice(1)}
+                    onClick={() => {
+                        setTheme(themeOption)
+                    }}
+                    value={themeOption} 
+                />
+            )))}
+        </div>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-import Image from 'next/image'
+import { ThemeToggle } from './components/theme-toggle'
 
 export default function Home() {
   return (
-    <main>
-      Blog
+    <main className="max-w-2xl mx-auto py-12 p-4">
+      <div className='flex flex-col gap-6'>
+        <ThemeToggle />
+
+        Blog
+        <button className="btn btn-primary">Button</button>
+      </div>
     </main>
   )
 }
