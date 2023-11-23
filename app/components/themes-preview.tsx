08c84aab0cf59379a26c5fdc1179e94d5597f59d
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-11-23 12:15:45 +0000

    renamed component, lucide icons, refactor

--- app/components/themes-preview.tsx
+++ app/components/themes-preview.tsx
@@ -2,7 +2,7 @@
 
 import { useTheme } from "next-themes"
 
-export const ThemeToggle = () => {
+export const ThemesPreview = () => {
     const { theme, setTheme } = useTheme()
 
     const themes = [
@@ -14,13 +14,13 @@ export const ThemeToggle = () => {
     ];
 
     return(
-        <div className="join join-vertical max-w-[8rem]">
+        <div className="flex flex-wrap gap-4">
             {themes.map((themeOption => (
                 <input 
                     key={themeOption }
                     type="radio" 
                     name="theme-buttons" 
-                    className="btn theme-controller join-item" 
+                    className="btn" 
                     checked={ theme === themeOption } 
                     // capitalize theme name
                     aria-label={themeOption.charAt(0).toUpperCase()+ themeOption.slice(1)}
@@ -30,6 +30,19 @@ export const ThemeToggle = () => {
                     value={themeOption} 
                 />
             )))}
+            <div className="bg-base-200 mt-8 p-8 w-full rounded-[var(--rounded-box)]">
+                <div className="flex flex-wrap gap-3">
+                    <button className="btn btn-primary">
+                        Primary button
+                    </button>
+                    <button className="btn btn-secondary">
+                        Secondary button
+                    </button>
+                    <button className="btn btn-accent">
+                        Accent button
+                    </button>
+                </div>
+            </div>
         </div>
     );
 }
