62f1dfbe30b65b93a0811f1b4be159461a8b57ef
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-11-23 12:15:11 +0000

    renamed route

---
+++ app/themes/page.tsx
@@ -0,0 +1,9 @@
+import { ThemesPreview } from "../components/themes-preview";
+
+export default function Themes() {
+    return(
+        <main className="py-12">
+            <ThemesPreview />
+        </main>     
+    );
+}
