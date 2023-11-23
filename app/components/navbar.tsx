1609f63f1be02d6afcbc39bebbff5e93155176f5
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-11-23 14:01:38 +0000

    navbar menu

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,24 +1,61 @@
-import { Palette } from "lucide-react"
+'use client'
+
 import Link from "next/link"
+import { useState } from "react"
 import { ThemeToggle } from "./theme-toggle"
+import { MoreHorizontal, X } from "lucide-react"
+
+
+const links = [
+    {title: 'Themes', path: '/themes'},
+    {title: 'FAQ', path: '/faq'},
+    {title: 'Docs', path: '/docs'},
+]
 
 export const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleMenu = () => setIsOpen(!isOpen);
+
+
     return(
-        <div className="navbar px-4 bg-primary text-primary-content rounded-[var(--rounded-box)] my-3">
+        <div className="px-4 bg-primary text-primary-content rounded-[var(--rounded-box)] my-3">
+            <div className="navbar">
             <Link 
                 href="/"
                 className="font-semibold text-lg">
                     Yu
             </Link>
             <div className="ms-auto">
-                <ThemeToggle />
-                <button className="btn btn-square btn-ghost">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
+                {/* <ThemeToggle /> */}
+                <button 
+                    onClick={() => {
+                        toggleMenu()
+                    }}
+                    className="btn btn-square btn-ghost"
+                >
+                    {isOpen ? <X /> : <MoreHorizontal /> }
                 </button>
-                <Link className="btn btn-square btn-ghost" href="/themes" title="Themes">
+                {/* <Link className="btn btn-square btn-ghost" href="/themes" title="Themes">
                     <Palette />
-                </Link>
+                </Link> */}
+
             </div>
+            </div>
+            {isOpen && (
+                <div className="pb-6 text-center font-semibold">
+                    <nav>
+                        <ul className="space-y-3">
+                            {links.map((link, index) => (
+                                <li key={index} className="hover:underline">
+                                    <Link href={link.path}>
+                                        {link.title}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </nav>
+                </div>
+            )}
         </div>
     )
 }
