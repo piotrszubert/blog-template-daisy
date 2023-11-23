90fa0c20c4341d6ea515eed8dabe9d2266fd9b8b
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-11-23 19:00:23 +0100

    added hero component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Link from "next/link"
 import { PostList } from "./components/posts-list"
+import { Hero } from "./components/hero"
 
 export default function Home() {
   return (
     <main className="py-12 min-h-screen">
+      <Hero />
       <div className="flex flex-col gap-6">
         <PostList />
       </div>
