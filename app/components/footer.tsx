64c992ee1039023302c8cc1c7bcacba2dec672c4
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-11-23 22:41:10 +0100

    ghost buttons in footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -11,21 +11,21 @@ export const Footer = () => {
         <div className="grid grid-flow-col gap-4">
           <a
             href="#"
-            className="transition-transform duration-200 hover:-translate-y-1"
+            className="transition-transform duration-200 hover:-translate-y-1 hover:scale-125 btn btn-ghost "
           >
             <Mail />
           </a>
           <a
             href="#"
-            className="transition-transform duration-200 hover:-translate-y-1"
+            className="transition-transform duration-200 hover:-translate-y-1 hover:scale-125 btn btn-ghost"
           >
             <Twitter />
           </a>
           <a
             href="#"
-            className="transition-transform duration-200 hover:-translate-y-1"
+            className="transition-transform duration-200 hover:-translate-y-1 hover:scale-125 btn btn-ghost"
           >
-            <Github />
+            <Github width="" />
           </a>
         </div>
       </nav>
