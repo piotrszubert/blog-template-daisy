d4b57527e88358a6ae60da89b48659a69ceae0da
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-11-24 09:09:26 +0000

    font from theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,8 @@
 import type { Metadata } from "next"
-import { Inter } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "./components/theme-provider"
 import { Footer } from "./components/footer"
 import { Navbar } from "./components/navbar"
-
-const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className}`}>
+      <body>
         <div className="mx-auto max-w-2xl px-4">
           <ThemeProvider
             attribute="data-theme"
