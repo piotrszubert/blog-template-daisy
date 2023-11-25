beb56512ad406d17480378bf44b0c08979f565fd
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-11-25 20:10:27 +0100

    added example post route

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,3 +1,11 @@
 export async function GET() {
     return Response.json("hello")
 }
+
+export async function POST(request: Request) {
+    const formData = await request.formData()
+    const name = formData.get('name')
+    const email = formData.get('email')
+    return Response.json({ name, email })
+}
+  
