16ef237bd601b871c2aa6309ad13b7214e6a9030
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-11-26 16:33:23 +0100

    added shadow on footer icons

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -12,7 +12,7 @@ export const Footer = () => {
         <div className="grid grid-flow-col gap-4">
           <a
             href={`mailto:${siteConfig.links.mail}`}
-            className="btn btn-ghost transition-transform duration-200 hover:-translate-y-1 hover:scale-125 "
+            className="btn btn-ghost transition-transform duration-200 hover:-translate-y-1 hover:scale-125 hover:shadow-lg "
           >
             <Mail width="18" />
           </a>
@@ -20,7 +20,7 @@ export const Footer = () => {
             href={siteConfig.links.twitter}
             target="_blank"
             rel="noopener noreferrer"
-            className="btn btn-ghost transition-transform duration-200 hover:-translate-y-1 hover:scale-125"
+            className="btn btn-ghost transition-transform duration-200 hover:-translate-y-1 hover:scale-125 hover:shadow-lg"
           >
             <Twitter width="18" />
           </a>
@@ -28,7 +28,7 @@ export const Footer = () => {
             href={siteConfig.links.github}
             target="_blank"
             rel="noopener noreferrer"
-            className="btn btn-ghost transition-transform duration-200 hover:-translate-y-1 hover:scale-125"
+            className="btn btn-ghost transition-transform duration-200 hover:-translate-y-1 hover:scale-125 hover:shadow-lg"
           >
             <Github width="18" />
           </a>
