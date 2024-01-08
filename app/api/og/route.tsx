88d04f6dfd75016b02d296a57c28f3e58455b6b7
ps777 <56358194+piotrszubert@users.noreply.github.com>
2024-01-08 21:29:55 +0100

    api route - generate og image

---
+++ app/api/og/route.tsx
@@ -0,0 +1,31 @@
+// file extension must be jsx/tsx
+
+import { ImageResponse } from 'next/og';
+ 
+export const runtime = 'edge';
+ 
+export async function GET() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 40,
+          color: 'black',
+          background: 'white',
+          width: '100%',
+          height: '100%',
+          padding: '50px 200px',
+          textAlign: 'center',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        Hello 你好 नमस्ते こんにちは สวัสดีค่ะ 안녕 добрий день Hallá 
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+    },
+  );
+}
