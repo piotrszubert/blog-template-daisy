31e78211f0ad0dad4b259ed0a548bb335612a12a
ps777 <56358194+piotrszubert@users.noreply.github.com>
2024-01-08 21:59:50 +0100

    sitemap, og image, config url

--- app/robots.ts
+++ app/robots.ts
@@ -1,5 +1,6 @@
 import { MetadataRoute } from 'next'
- 
+import { siteConfig } from '@/config/site'
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
@@ -7,6 +8,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: ['/', '/api/og/*'],
     //   disallow: '/private/',
     },
-    // sitemap: 'https://acme.com/sitemap.xml',
+    sitemap: `${siteConfig.site.url}/sitemap.xml`,
   }
 }
